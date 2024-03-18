f108b7193517f07caf10e268fb91da1cc7e24420
H4N4K8 <123414820+H4N4K8@users.noreply.github.com>
2024-03-18 15:27:18 -0400

    Add files via upload

---
+++ project/js/project.js
@@ -0,0 +1,4 @@
+// menu buttons = go to game link
+$('#game1').on('click', function () { location.assign("tictactoe.html"); });
+$('#game2').on('click', function () { location.assign("snake.html"); }); //"game" currently placeholders
+$('#game3').on('click', function () { location.assign("pong.html"); });
